979ac8880c6e7c20e6b24607ca9192e1a92870bc
Shota Tamura <tamura@yuuniworks.com>
2026-05-02 14:40:41 +0900

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 // @ts-check
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
@@ -8,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.yuuniworks.com',
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
